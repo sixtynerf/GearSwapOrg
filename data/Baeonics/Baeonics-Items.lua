4e1b37f11a6c509e9696b93cbe86fcc14613a1d6
whatof <79544158+sixtynerf@users.noreply.github.com>
2025-02-17 12:04:46 +1100

    Create Baeonics-Items.lua

---
+++ data/Baeonics/Baeonics-Items.lua
@@ -0,0 +1,136 @@
+--Gearsets your character will use among multiple jobs.
+sets.BehemothSuit = {body="Behemoth Suit +1",hands=empty,legs=empty,feet=empty}
+sets.Sheltered = {ring2="Sheltered Ring"}
+sets.Capacity = {back="Mecisto. Mantle"}
+
+--sets.buff.Doom = {waist="Gishdubar Sash"} --neck="Nicander's Necklace",ring1="Eshmun's Ring",ring2="Eshmun's Ring"
+sets.buff.Doom = {neck="Nicander's Necklace",waist="Gishdubar Sash",ring1="Eshmun's Ring",ring2="Purity Ring"}
+--sets.TreasureHunter = {head="Volte Cap",hands="Volte Bracers"}
+sets.TreasureHunter = {head="Wh. Rarab Cap +1",waist="Chaac Belt",ammo="Per. Lucky Egg"}
+sets.Cure_Received = {neck="Phalaina Locket",ring1="Kunaji Ring",ring2="Asklepian Ring",waist="Gishdubar Sash"}
+
+sets.precast.Item['Hallowed Water'] = {}
+sets.precast.Item['Holy Water'] = {}
+
+--Augmented items that you'll use among multiple jobs.
+gear.grioavolr_nuke_staff = {name="Grioavolr",augments={'"Conserve MP"+2','MP+83','Mag. Acc.+25','"Mag.Atk.Bns."+29','Magic Damage +5',}}
+gear.grioavolr_fc_staff = {name="Grioavolr",augments={'"Fast Cast"+7','MND+4','Mag. Acc.+21','"Mag.Atk.Bns."+24','Magic Damage +2',}}
+
+gear.chironic_nuke_head = {name="Chironic Hat",augments={'Mag. Acc.+25 "Mag.Atk.Bns."+25','"Drain" and "Aspir" potency +9','INT+7','Mag. Acc.+14','"Mag.Atk.Bns."+7',}}
+gear.chironic_aspir_gloves = {name="Chironic Gloves", augments={'"Drain" and "Aspir" potency +10','AGI+1','Mag. Acc.+14 "Mag.Atk.Bns."+14',}}
+gear.chironic_nuke_body = {name="Chironic Doublet",augments={'Mag. Acc.+18 "Mag.Atk.Bns."+18','CHR+9','Mag. Acc.+13','"Mag.Atk.Bns."+9',}}
+gear.chironic_refresh_hands = { name="Chironic Gloves", augments={'Attack+13','"Dual Wield"+2','"Refresh"+2','Mag. Acc.+5 "Mag.Atk.Bns."+5',}}
+gear.chironic_nuke_body = {name="Chironic Doublet",augments={'Mag. Acc.+18 "Mag.Atk.Bns."+18','CHR+9','Mag. Acc.+13','"Mag.Atk.Bns."+9',}}
+gear.chironic_nuke_hands = {name="Chironic Gloves",augments={'Mag. Acc.+19 "Mag.Atk.Bns."+19','"Cure" potency +9%','INT+7','Mag. Acc.+7','"Mag.Atk.Bns."+12',}}
+gear.chironic_macc_legs = {name="Chironic Hose",augments={'Mag. Acc.+19 "Mag.Atk.Bns."+19','"Fast Cast"+1','MND+7','Mag. Acc.+15',}}
+gear.chironic_aspir_legs = {name="Chironic Hose",augments={'Mag. Acc.+17 "Mag.Atk.Bns."+17','"Drain" and "Aspir" potency +10','MND+7','Mag. Acc.+14','"Mag.Atk.Bns."+9',}}
+gear.chironic_treasure_feet = {name="Chironic Slippers",augments={'Pet: "Mag.Atk.Bns."+26','INT+15','"Treasure Hunter"+1','Mag. Acc.+17 "Mag.Atk.Bns."+17',}}
+gear.chironic_refresh_feet = {name="Chironic Slippers",augments={'INT+9','"Dbl.Atk."+1','"Refresh"+1','Mag. Acc.+7 "Mag.Atk.Bns."+7',}}
+
+gear.merlinic_refresh_feet = {name="Merlinic Crackows",augments={'STR+8','Rng.Atk.+1','"Refresh"+2','Accuracy+6 Attack+6','Mag. Acc.+10 "Mag.Atk.Bns."+10',}}
+gear.merlinic_treasure_legs = {name="Merlinic Shalwar",augments={'Accuracy+25 Attack+25','Magic dmg. taken -1%','"Treasure Hunter"+2',}}
+gear.merlinic_refresh_legs = {name="Merlinic Shalwar",augments={'Rng.Acc.+19','Pet: INT+4','"Refresh"+1',}}
+
+--Odyssey Gear
+BUMBA_HEAD = {name="Nyame Helm"}
+BUMBA_BODY = {name="Nyame Mail"}
+BUMBA_HANDS = {name="Nyame Gauntlets"}
+BUMBA_LEGS = {name="Nyame Flanchard"}
+BUMBA_FEET = {name="Nyame Sollerets"}
+
+ONGO_HEAD = {name="Agwu's Cap"}
+ONGO_BODY = {name="Agwu's Robe"}
+ONGO_HANDS = {name="Agwu's Gages"}
+ONGO_LEGS = {name="Agwu's Slops"}
+ONGO_FEET = {name="Agwu's Pigaches"}
+
+XEVIOSO_HEAD = {name="Ikenga's Hat"}
+XEVIOSO_BODY = {name="Ikenga's Vest"}
+XEVIOSO_HANDS = {name="Ikenga's Gloves"}
+XEVIOSO_LEGS = {name="Ikenga's Trousers"}
+XEVIOSO_FEET = {name="Ikenga's Clogs"}
+
+NGAI_HEAD = {name="Gleti's Mask"}
+NGAI_BODY = {name="Gleti's Cuirass"}
+NGAI_HANDS = {name="Gleti's Gauntlets"}
+NGAI_LEGS = {name="Gleti's Breeches"}
+NGAI_FEET = {name="Gleti's Boots"}
+
+KALUNGA_HEAD = {name="Sakpata's Helm"}
+KALUNGA_BODY = {name="Sakpata's Breastplate"}
+KALUNGA_HANDS = {name="Sakpata's Gauntlets"}
+KALUNGA_LEGS = {name="Sakpata's Cuisses"}
+KALUNGA_FEET = {name="Sakpata's Leggings"}
+
+MBOZE_HEAD = {name="Bunzi's Hat"}
+MBOZE_BODY = {name="Bunzi's Robe"}
+MBOZE_HANDS = {name="Bunzi's Gloves"}
+MBOZE_LEGS = {name="Bunzi's Pants"}
+MBOZE_FEET = {name="Bunzi's Sabots"}
+
+AREBATI_HEAD = {name="Mpaca's Cap"}
+AREBATI_BODY = {name="Mpaca's Doublet"}
+AREBATI_HANDS = {name="Mpaca's Gloves"}
+AREBATI_LEGS = {name="Mpaca's Hose"}
+AREBATI_FEET = {name="Mpaca's Boots"}
+
+--Ambuscade Gear
+SULEVIA_HEAD = {name="Sulevia's Mask +2"}
+SULEVIA_BODY = {name="Sulevia's Platemail +2"}
+SULEVIA_HANDS = {name="Sulev. Gauntlets +2"}
+SULEVIA_LEGS = {name="Sulev. Cuisses +2"}
+SULEVIA_FEET = {name="Sulevia's Leggings +2"}
+
+HIZAMARU_HEAD = {name="Hizamaru Somen +2"}
+HIZAMARU_BODY = {name="Hizamaru Haramaki +2"}
+HIZAMARU_HANDS = {name="Hizamaru Kote +2"}
+HIZAMARU_LEGS = {name="Hizamaru Hizayoroi +2"}
+HIZAMARU_FEET = {name="Hizamaru Sune-Ate +2"}
+
+INYANGA_HEAD = {name="Inyanga Tiara +2"}
+INYANGA_BODY = {name="Inyanga Jubbah +2"}
+INYANGA_HANDS = {name="Inyanga Dastanas +2"}
+INYANGA_LEGS = {name="Inyanga Shalwar +2"}
+INYANGA_FEET = {name="Inyanga Crackows +2"}
+
+MEGHANADA_HEAD = {name="Meghanada Visor +2"}
+MEGHANADA_BODY = {name="Meghanada Cuirie +2"}
+MEGHANADA_HANDS = {name="Meghanada Gloves +2"}
+MEGHANADA_LEGS = {name="Meghanada Chausses +2"}
+MEGHANADA_FEET = {name="Meghanada Jambeaux +2"}
+
+JHAKRI_HEAD = {name="Jhakri Coronal +2"}
+JHAKRI_BODY = {name="Jhakri Robe +2"}
+JHAKRI_HANDS = {name="Jhakri Cuffs +2"}
+JHAKRI_LEGS = {name="Jhakri Slops +2"}
+JHAKRI_FEET = {name="Jhakri Pigaches +2"}
+
+FLAMMA_HEAD = {name="Flam. Zucchetto +2"}
+FLAMMA_BODY = {name="Flamma Korazin +2"}
+FLAMMA_HANDS = {name="Flam. Manopolas +2"}
+FLAMMA_LEGS = {name="Flamma Dirs +2"}
+FLAMMA_FEET = {name="Flam. Gambieras +2"} 
+
+TALIAH_HEAD = {name="Tali'ah Turban +2"}
+TALIAH_BODY = {name="Tali'ah Manteel +2"}
+TALIAH_HANDS = {name="Tali'ah Gages +2"}
+TALIAH_LEGS = {name="Tali'ah Seraweels +2"}
+TALIAH_FEET = {name="Tali'ah Crackows +2"}
+
+MUMMU_HEAAD = {name="Mummu Bonnet +2"}
+MUMMU_BODY = {name="Mummu Jacket +2"}
+MUMMU_HANDS = {name="Mummu Wrist +2"}
+MUMMU_LEGS = {name="Mummu Kecks +2"}
+MUMMU_FEET = {name="Mummu Gamashes +2"}
+
+AYANMO_HEAD = {name="Ayanmo Zucchetto +2"}
+AYANMO_BODY = {name="Ayanmo Corazza +2"}
+AYANMO_HANDS = {name="Ayanmo Manopolas +2"}
+AYANMO_LEGS = {name="Ayanmo Cosciales +2"}
+AYANMO_FEET = {name="Ayanmo Gambieras +2"}
+
+MALLQUIS_HEAD = {name="Mallquis Chapeau +2"}
+MALLQUIS_BODY = {name="Mallquis Saio +2"}
+MALLQUIS_HANDS = {name="Mallquis Cuffs +2"}
+MALLQUIS_LEGS = {name="Mallquis Trews +2"}
+MALLQUIS_FEET = {name="Mallquis Clogs +2"}
