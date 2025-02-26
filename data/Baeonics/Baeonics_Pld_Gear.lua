bf1a14912061ea0ff7cc3400a1c22650ff1ac1b6
whatof <79544158+sixtynerf@users.noreply.github.com>
2025-02-27 08:14:07 +1100

    Update Baeonics_Pld_Gear.lua

--- data/Baeonics/Baeonics_Pld_Gear.lua
+++ data/Baeonics/Baeonics_Pld_Gear.lua
@@ -19,23 +19,23 @@ function user_job_setup()
 --Artifact Gear
 	AF_HEAD 		= {name="Rev. Coronet +1"}			--no			
 	AF_BODY 		= {name="Rev. Surcoat +1"}			--yes	
-	AF_HANDS 		= {name="Rev. Gauntlets +1"}		--yes		
+	AF_HANDS 		= {name="Rev. Gauntlets +1"}			--yes		
 	AF_LEGS 		= {name="Rev. Breeches +1"}			--yes			
 	AF_FEET 		= {name="Rev. Leggings +1"}			--yes
 
 --Relic Gear
 	RELIC_HEAD 		= {name="Cab. Coronet +1"}			--yes		
 	RELIC_BODY 		= {name="Cab. Surcoat +1"}			--yes		
-	RELIC_HANDS 		= {name="Cab. Gauntlets +1"}	--yes
+	RELIC_HANDS 		= {name="Cab. Gauntlets +1"}			--yes
 	RELIC_LEGS 		= {name="Cab. Breeches +1"}			--YMMV
 	RELIC_FEET 		= {name="Cab. Leggings +1"}			--yes	
 
 --Empyrean Gear
 	EMPY_HEAD 		= {name="Chev. Armet +3"}			--yes!		
 	EMPY_BODY 		= {name="Chev. Cuirass +3"}			--yes		
-	EMPY_HANDS 		= {name="Chev. Gauntlets +3"}		--maybe		
+	EMPY_HANDS 		= {name="Chev. Gauntlets +3"}			--maybe		
 	EMPY_LEGS 		= {name="Chev. Cuisses +3"}			--yes!		
-	EMPY_FEET 		= {name="Chev. Sabatons +3"}		--yes
+	EMPY_FEET 		= {name="Chev. Sabatons +3"}			--yes
 
 --Cursed Gears
 	--pathC
