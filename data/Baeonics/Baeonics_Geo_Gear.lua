8f0a54b334fc99a36d25c7e813bef4679bc3fb4b
whatof <79544158+sixtynerf@users.noreply.github.com>
2025-04-02 06:34:05 +1100

    Update Baeonics_Geo_Gear.lua

--- data/Baeonics/Baeonics_Geo_Gear.lua
+++ data/Baeonics/Baeonics_Geo_Gear.lua
@@ -15,6 +15,7 @@ function user_job_setup()
 
 	autoindi = "Haste"
 	autogeo = "Frailty"
+	autows = "Black Halo"
 	
 --AMBU CAPES
 	AMBUNUKE_BACK = {name="Nantosuelta's Cape",augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Mag.Atk.Bns."+10'}}
@@ -649,36 +650,80 @@ sets.precast.FC =
 
 	sets.idle.Weak = 
 	{
-		main="Bolelabunga",sub="Genmei Shield",ammo="Staunch Tathlum +1",
-		head="Befouled Crown",neck="Loricate Torque +1",ear1="Etiolation Earring",ear2="Ethereal Earring",
-		body="Jhakri Robe +2",hands=gear.merlinic_refresh_hands,ring1="Defending Ring",ring2="Dark Ring",
-		back="Umbra Cape",waist="Carrier's Sash",legs="Assid. Pants +1",feet=EMPY_FEET
+		main="Bolelabunga",
+		sub="Genmei Shield",
+		ammo="Staunch Tathlum +1",
+		head="Befouled Crown",
+		neck="Loricate Torque +1",
+		ear1="Etiolation Earring",
+		ear2="Ethereal Earring",
+		body=BUMBA_BODY,
+		hands=BUMBA_HANDS,
+		ring1="Defending Ring",
+		ring2="Dark Ring",
+		back="Umbra Cape",
+		waist="Carrier's Sash",
+		legs="Assid. Pants +1",
+		feet=EMPY_FEET
 	}
 
 	-- Defense sets
 	
 	sets.defense.PDT = 
 	{
-		main="Malignance Pole",sub="Umbra Strap",ammo="Staunch Tathlum +1",
-		head=BUMBA_HEAD,neck="Loricate Torque +1",ear1="Etiolation Earring",ear2="Handler's Earring +1",
-		body="Mallquis Saio +2",hands=BUMBA_HANDS,ring1="Defending Ring",ring2="Dark Ring",
-		back="Umbra Cape",waist="Carrier's Sash",legs=BUMBA_LEGS,feet=EMPY_FEET
+		main="Malignance Pole",
+		sub="Umbra Strap",
+		ammo="Staunch Tathlum +1",
+		head=BUMBA_HEAD,
+		neck="Loricate Torque +1",
+		ear1="Etiolation Earring",
+		ear2="Handler's Earring +1",
+		body=BUMBA_BODY,
+		hands=BUMBA_HANDS,
+		ring1="Defending Ring",
+		ring2="Dark Ring",
+		back="Umbra Cape",
+		waist="Carrier's Sash",
+		legs=BUMBA_LEGS,
+		feet=EMPY_FEET
 	}
 
 	sets.defense.MDT = 
 	{
-		main="Malignance Pole",sub="Umbra Strap",ammo="Staunch Tathlum +1",
-		head=EMPY_HEAD,neck="Loricate Torque +1",ear1="Etiolation Earring",ear2="Handler's Earring +1",
-		body="Mallquis Saio +2",hands=BUMBA_HANDS,ring1="Defending Ring",ring2="Dark Ring",
-		back="Umbra Cape",waist="Carrier's Sash",legs=BUMBA_LEGS,feet=EMPY_FEET
+		main="Malignance Pole",
+		sub="Umbra Strap",
+		ammo="Staunch Tathlum +1",
+		head=EMPY_HEAD,
+		neck="Loricate Torque +1",
+		ear1="Etiolation Earring",
+		ear2="Handler's Earring +1",
+		body=BUMBA_BODY,
+		hands=BUMBA_HANDS,
+		ring1="Defending Ring",
+		ring2="Dark Ring",
+		back="Umbra Cape",
+		waist="Carrier's Sash",
+		legs=BUMBA_LEGS,
+		feet=EMPY_FEET
 	}
 
 	sets.defense.MEVA = 
 	{
-		main="Malignance Pole",sub="Enki Strap",ammo="Staunch Tathlum +1",
-		head=EMPY_HEAD,neck="Warder's Charm +1",ear1="Etiolation Earring",ear2="Sanare Earring",
-		body=gear.merlinic_nuke_body,hands="Telchine Gloves",ring1="Vengeful Ring",Ring2="Purity Ring",
-		back=AMBUIDLE_BACK,waist="Luminary Sash",legs="Telchine Braconi",feet=EMPY_FEET
+		main="Malignance Pole",
+		sub="Enki Strap",
+		ammo="Staunch Tathlum +1",
+		head=EMPY_HEAD,
+		neck="Warder's Charm +1",
+		ear1="Etiolation Earring",
+		ear2="Sanare Earring",
+		body=BUMBA_BODY,
+		hands="Telchine Gloves",
+		ring1="Vengeful Ring",
+		Ring2="Purity Ring",
+		back=AMBUIDLE_BACK,
+		waist="Luminary Sash",
+		legs="Telchine Braconi",
+		feet=EMPY_FEET
 	}
 		
 	sets.defense.PetPDT = sets.idle.PDT.Pet
@@ -697,23 +742,30 @@ sets.precast.FC =
 	
 	sets.HPDown = 
 	{
-		head="Pixie Hairpin +1",ear1="Mendicant's Earring",ear2="Evans Earring",
-		body="Jhakri Robe +2",hands="Jhakri Cuffs +2",ring1="Mephitas's Ring +1",ring2="Mephitas's Ring",
-		back="Swith Cape +1",legs="Shedir Seraweels",feet="Jhakri Pigaches +2"
+		head="Pixie Hairpin +1",
+		ear1="Mendicant's Earring",
+		ear2="Evans Earring",
+		body=BUMBA_BODY,
+		hands=BUMBA_HANDS,
+		ring1="Mephitas's Ring +1",
+		ring2="Mephitas's Ring",
+		back="Swith Cape +1",
+		legs=BUMBA_LEGS,
+		feet=BUMBA_FEET
 	}
 
 	sets.buff.Doom = set_combine(sets.buff.Doom, {})
 
-	--------------------------------------
-	-- Engaged sets
-	--------------------------------------
-	
-	-- Variations for TP weapon and (optional) offense/defense modes.  Code will fall back on previous
-	-- sets if more refined versions aren't defined.
-	-- If you create a set with both offense and defense modes, the offense mode should be first.
-	-- EG: sets.engaged.Dagger.Accuracy.Evasion
+--------------------------------------
+-- Engaged sets
+--------------------------------------
 
-	-- Normal melee group
+-- Variations for TP weapon and (optional) offense/defense modes.  Code will fall back on previous
+-- sets if more refined versions aren't defined.
+-- If you create a set with both offense and defense modes, the offense mode should be first.
+-- EG: sets.engaged.Dagger.Accuracy.Evasion
+
+-- Normal melee group
 	sets.engaged = 
 	{
 		ammo="Hasty Pinion +1",
@@ -734,19 +786,28 @@ sets.precast.FC =
 	sets.engaged.DW = 
 	{
 		ammo="Hasty Pinion +1",
-		head="Befouled Crown",neck="Asperity Necklace",ear1="Dudgeon Earring",ear2="Heartseeker Earring",
-		body="Jhakri Robe +2",hands="Regal Cuffs",ring1="Ramuh Ring +1",ring2="Ramuh Ring +1",
-		back="Moonlight Cape",waist="Witful Belt",legs="Assid. Pants +1",feet="Battlecast Gaiters"
+		head="Befouled Crown",
+		neck="Asperity Necklace",
+		ear1="Dudgeon Earring",
+		ear2="Heartseeker Earring",
+		body=BUMBA_BODY,
+		hands="Regal Cuffs",
+		ring1="Ramuh Ring +1",
+		ring2="Ramuh Ring +1",
+		back="Moonlight Cape",
+		waist="Witful Belt",
+		legs="Assid. Pants +1",
+		feet="Battlecast Gaiters"
 	}
 
-	--------------------------------------
-	-- Custom buff sets
-	--------------------------------------
-	
-	-- Gear that converts elemental damage done to recover MP.	
-	sets.RecoverMP = {body="Seidr Cotehardie"}
-	
-	-- Gear for Magic Burst mode.
+--------------------------------------
+-- Custom buff sets
+--------------------------------------
+
+-- Gear that converts elemental damage done to recover MP.	
+sets.RecoverMP = {body="Seidr Cotehardie"}
+
+-- Gear for Magic Burst mode.
 	sets.MagicBurst = 
 	{
 		main="Bunzi's Rod",
